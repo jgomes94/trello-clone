af832af5256cce2ce45354373342d6204b84fb31
João <gomes.jfn@gmail.com>
2020-12-23 12:12:46 +0000

    Drag and Drop cards between boards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,97 @@
+import React from 'react';
 import './App.css';
+
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import CardDisplay from './card-display/CardDisplay';
 
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+    this.state.columnList =  [{
+      id: 100,
+      title:'TODO',
+      cardList:[{id: 1, order: 1, title:'Do something productive', body:"clean the dishes"}]
+    },{
+      id: 101,
+      title:'DOING',
+      cardList:[{id: 2, order: 1, title:'Call the Delivery Company', body:"clean the dishes"}]
+    },{
+      id: 102,
+      title:'DONE',
+      cardList:[{id: 3, order: 1, title:'Wake Up', body:"clean the dishes"},
+                {id: 4, order: 2, title:'Get Dressed', body:"Make the bed"},
+                {id: 5, order: 3, title:'Make Coffee', body:"Make Coffee"}]
+    }];
+  }
 
-
-
-
-
-function App() {
-  let cardList = [{
-    title:'TODO',
-    cardList:[{title:'Do something productive', body:"clean the dishes"}]
-  },{
-    title:'DOING',
-    cardList:[{title:'Call the Delivery Company', body:"clean the dishes"}]
-  },{
-    title:'DONE',
-    cardList:[{title:'Wake Up', body:"clean the dishes"}]
-  }];
-  
-
-
-  return (
-    <div className="App">
-      <div className="row">
-        {
-          cardList.map((el) =>  {
-            return (<div className='card-column'>
-              <CardDisplay title={el.title} cardList={el.cardList}/>
-            </div>)
-          })
-        } 
+  render() {
+      return (
+        <div className="App">
+          <div className="row">
+            <DragDropContext onDragEnd={this.updateCards.bind(this)}>
+                  {this.state.columnList.map((el) => (
+                    <Droppable key={''+el.id} droppableId={''+el.id}>
+                      {(provided) => (
+                        <div ref={provided.innerRef} className='card-column'>
+                                <CardDisplay 
+                                  {...provided.droppableProps}
+                                  {...provided.placeholder}
+                                  className="characters"
+                                  title={el.title} 
+                                  cardList={el.cardList}/>
+                          {provided.placeholder}
+                        </div>
+                      )}
+                      </Droppable> 
+                    ))}
+            </DragDropContext>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
+
+  updateCards(result) {
+    const card = this.findCardById(result.draggableId);
+    if(card && result && result.destination && result.destination.droppableId){
+      this.removeCardById(result.draggableId);
+      this.addCardToTable(card, result.destination.droppableId, result.destination.index);
+    }
+  }
+  
+  findCardById(cardId) {
+    for(let column of this.state.columnList){
+      for(let card of column.cardList){  
+        if(Number(card.id) ===  Number(cardId)){
+          return card;
+        }
+      }
+    }
+  
+    return null;
+  }
+  
+  removeCardById(cardId) {
+    for(let column of this.state.columnList) {
+      column.cardList = column.cardList.filter((el) => {
+        return Number(el.id) !== Number(cardId)});
+    }
+  } 
+
+  addCardToTable(card, tableId, order) {
+    for(let column of this.state.columnList) {
+      if(Number(column.id) === Number(tableId)) {
+        column.cardList.splice(order, 0, card);
+        return;
+      }
+    }
+
+  }
+
 }
+
+
+
+
 
 export default App;
--- src/components/App/card-display/CardDisplay.js
+++ src/components/App/card-display/CardDisplay.js
@@ -1,24 +1,39 @@
+import React from 'react';
+
 import './CardDisplay.css';
+import { Draggable  } from 'react-beautiful-dnd';
 
+class CreateCard extends React.Component {
+    render(){
+            const cardList = this.props.cardList
+            return (
+            <div className="card-container">
+                <div className="card-header">
+                    <span className="card-header">{this.props.title}</span>
+                </div>
+                {
+                    cardList.map((el, index)=>{
+                        return(
+                            <Draggable key={el.id} draggableId={''+el.id} index={index}>
+                                {(provided) => (
+                                    <div ref={provided.innerRef} 
+                                        {...provided.draggableProps} 
+                                        {...provided.dragHandleProps} 
+                                        className="card-body">
+                                        <div className="card-thumbnail">
+                                            <span>{el.title} </span>
+                                        </div>
+                                    </div>
+                                )}
+                            </Draggable>
+                        )
+                    })
+                }
+                
+            </div>
+            );
 
-function CreateCard({title, cardList}) {
-    return (
-    <div className="card-container">
-        <div className="card-header">
-            <span className="card-header">{title}</span>
-        </div>
-        {
-            cardList.map((el)=>{
-                return(<div className="card-body">
-                    <div className="card-thumbnail">
-                        <span>{el.title} </span>
-                    </div>
-                </div>)
-            })
-        }
-        
-    </div>
-    );
+    }
 }
 
 export default CreateCard;
